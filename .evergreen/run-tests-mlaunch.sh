854ccd45411a9e757ba2a6d3f8993d3ad894080c
Hana Pearlman <pearlmanhana@gmail.com>
2019-08-05 09:18:02 -0400

    test replacing files

--- .evergreen/run-tests-mlaunch.sh
+++ .evergreen/run-tests-mlaunch.sh
@@ -18,36 +18,53 @@ set -o errexit  # Exit the script with error if any of the commands fail
 set_fcv
 set_env_vars
 
-#export DRIVER_TOOLS_CLIENT_CERT_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/client-public.pem"
-#export DRIVER_TOOLS_CLIENT_KEY_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/client-private.pem"
-#export DRIVER_TOOLS_CLIENT_CERT_KEY_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/client.pem"
-#export DRIVER_TOOLS_CA_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/ca.pem"
-#export DRIVER_TOOLS_CLIENT_KEY_ENCRYPTED_PEM="${DRIVERS_TOOLS}/.evergreen/x509gen/password_protected.pem"
-
-if test -n "$SINGLE_MONGOS"; then
-  # Some tests may run into https://jira.mongodb.org/browse/SERVER-16836
-  # when executing against a multi-sharded mongos.
-  # At the same time, due to pinning in sharded transactions,
-  # it is beneficial to test a single shard to ensure that server
-  # monitoring and selection are working correctly and recover the driver's
-  # ability to operate in reasonable time after errors and fail points trigger
-  # on a single shard
-  echo Restricting to a single mongos
-  export MONGODB_URI=`echo "$MONGODB_URI" |sed -e 's/,.*//'`
-fi
-
 setup_ruby
 
 install_deps
 
+arch=ubuntu1404
+version=4.0.9
+prepare_server $arch $version
+
+install_mlaunch
+
+# Launching mongod under $MONGO_ORCHESTRATION_HOME
+# makes its long available through log collecting machinery
+
+export dbdir="$MONGO_ORCHESTRATION_HOME"/db
+mkdir -p "$dbdir"
+
+args="--setParameter enableTestCommands=1"
+args="$args --setParameter diagnosticDataCollectionEnabled=false"
+uri_options=
+if test "$TOPOLOGY" = replica_set; then
+  args="$args --replicaset --name ruby-driver-rs"
+elif test "$TOPOLOGY" = sharded_cluster; then
+  args="$args --replicaset --sharded 2 --name ruby-driver-rs"
+else
+  args="$args --single"
+fi
+if test -n "$MMAPV1"; then
+  args="$args --storageEngine mmapv1"
+  uri_options="$uri_options&retryReads=false&retryWrites=false"
+fi
+mlaunch --dir "$dbdir" --binarypath "$BINDIR" $args
+
 echo "Running specs"
 which bundle
 bundle --version
+
+export MONGODB_URI="mongodb://localhost:27017/?serverSelectionTimeoutMS=30000$uri_options"
+bundle exec rake spec:prepare
+
+export MONGODB_URI="mongodb://localhost:27017/?appName=test-suite$uri_options"
 bundle exec rake spec:ci
 test_status=$?
 echo "TEST STATUS"
 echo ${test_status}
 
 kill_jruby
+
+mlaunch stop --dir "$dbdir"
 
 exit ${test_status}
